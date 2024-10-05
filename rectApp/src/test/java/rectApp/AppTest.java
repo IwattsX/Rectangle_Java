794527ebc1b91c5404c71fd0e2fe37bb058621f9
Isaac Watts <Isaac.s.watts@gmail.com>
2024-10-05 11:18:03 -0400

    initial commit

---
+++ rectApp/src/test/java/rectApp/AppTest.java
@@ -0,0 +1,23 @@
+package rectApp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    // @Test
+    // public void shouldFail(){
+    //     assertTrue(false);
+    // }
+}
