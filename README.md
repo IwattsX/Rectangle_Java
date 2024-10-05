e83b89148022f6b4aba12eeecc08a0b3d403aa4d
Isaac Watts <Isaac.s.watts@gmail.com>
2024-10-05 11:47:39 -0400

    Modified the code to add commands inside of an ArrayList for future parsing into running the commands

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to run
+1) Get into the directory the app is in:
+```
+cd rectApp
+```
+2) build the package
+```
+mvn package
+```
+3) Run the code
+```
+java -cp target/rectApp-1.0-SNAPSHOT.jar rectApp.App PATH/*.cmd
+```
--- rectApp/src/main/java/rectApp/App.java
+++ rectApp/src/main/java/rectApp/App.java
@@ -1,10 +1,30 @@
 package rectApp;
 
-/**
- * Hello world!
- */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// give this an absolute filepath
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<String> commands = new ArrayList<String>();
+        try{
+            for(int i = 0; i<args.length; i++){
+                Scanner in = new Scanner(new File(args[i]));
+                
+                while(in.hasNextLine()){
+                    commands.add(in.nextLine());
+                }
+                in.close(); 
+            }
+        }
+        catch(Exception e){
+
+        }
+        finally{
+            System.out.println(commands);
+        }
     }
+    
 }
